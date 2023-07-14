f415274ac8250d8e545c66241e0e6ef66531f7d7
Alexandr <mrkobalt18@gmail.com>
2023-07-14 18:15:56 +0300

    solution

--- src/notebook/repository/GBRepository.java
+++ src/notebook/repository/GBRepository.java
@@ -1,17 +1,16 @@
 package notebook.repository;
 
+import notebook.model.User;
+
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Репозиторий, для выполнения CreateReadUpdateDelete (CRUD) операций
- * @param <E> тип модели данных
- * @param <I> тип ID модели данных E
- */
 public interface GBRepository<E, I> {
     List<E> findAll();
     E create(E e);
-    Optional<E> findById(I id);
+    Optional<E> findById(List<User> users, I id);
     Optional<E> update(I id, E e);
-    boolean delete(I id);
+    boolean delete(List<User> allUser, Long id);
+
+
 }
